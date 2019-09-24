33b121f958af4f8e6bcec7efb4a441f649654918
Raffaello <raffaello.chiarioni@gmail.com>
2019-09-24 23:41:33 +0200

    esercitazione 01 - Raffaello Chiarioni

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
+  background(10);
+  frameRate(60);
 }
 
 function draw() {
-  // put drawing code here
+
+  push();
+  translate(width/2, height/2);
+  rotate(frameCount);
+  stroke(color(frameCount/8));
+  strokeWeight(1);
+  line(0,200,50*cos(frameCount*3),50*-sin(frameCount*3));
+  pop();
+
+  push();
+  translate(width/2, height/2);
+  stroke(color(frameCount/8),frameCount/4,frameCount/2);
+  strokeWeight(1);
+  line(50,0,50*cos(frameCount*3),50*-sin(frameCount*3));
+  pop();
+
+  push();
+  translate(width/2, height/2);
+  rotate(frameCount);
+  stroke(color(frameCount/8));
+  strokeWeight(1);
+  line(0,200,50*-cos(frameCount*3),50*sin(frameCount*3));
+  pop();
+
+
 }
